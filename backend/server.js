37544c978e8402c6b771da775d176c608e886ea1
YANE20-19 <yaneliogonapaz50@gmail.com>
2026-02-20 14:49:13 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,16 @@
 const express = require("express");
-const cors = require("cors");
-const path = require("path");
-require("dotenv").config();
-
 const app = express();
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-// FRONTEND
-app.use(express.static(path.join(__dirname, "../frontend")));
+app.get("/", (req, res) => {
+  res.send("🚀 Backend Destino Tu Mundo activo");
+});
 
-// API
 app.use("/api/destinos", require("./routes/destinos"));
 app.use("/api/reservas", require("./routes/reservas"));
-
-// INDEX
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/index.html"));
-});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
