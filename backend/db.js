11e0dc7cff8e222e20eaaba62d9b14ba03b87cd5
YANE20-19 <yaneliogonapaz50@gmail.com>
2026-02-20 14:30:39 -0500

    hola

--- backend/db.js
+++ backend/db.js
@@ -5,8 +5,7 @@ const conexion = mysql.createConnection({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT,
-  ssl: { rejectUnauthorized: false }
+  port: process.env.MYSQLPORT
 });
 
 conexion.connect(err => {
